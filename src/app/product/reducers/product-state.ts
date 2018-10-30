38c70c9a984fc1bff0c0d5db94ca465ec737dcf3
Satpal Tanan <satpal.tanan@kelltontech.com>
2018-10-30 12:57:15 +0530

    Product reducers and actions

--- src/app/product/reducers/product-state.ts
+++ src/app/product/reducers/product-state.ts
@@ -2,4 +2,5 @@ import { Product } from '@shared/models/product.model';
 
 export interface ProductState {
   products: Product[];
+  selectedProduct: Product;
 }
--- src/app/product/reducers/product.reducer.ts
+++ src/app/product/reducers/product.reducer.ts
@@ -2,10 +2,16 @@ import { ProductActions } from './../actions/product.actions';
 import { ProductState } from './product-state';
 import { Action } from '@ngrx/store';
 
-export const initialState: ProductState = { products: [] };
+export const initialState: ProductState = { selectedProduct: null, products: [] };
 
-export function ProductReducer(state = initialState, action: Action) {
-  switch (action.type) {
+export function ProductReducer(state = initialState, { type, payload }: Action) {
+  switch (type) {
+    case ProductActions.GET_PRODUCT_DETAIL_SUCCESS: {
+      Object.assign({}, state, {
+        selectedProduct: payload
+      });
+      return state;
+    }
     case ProductActions.GET_ALL_PRODUCTS:
       return state;
     default:
