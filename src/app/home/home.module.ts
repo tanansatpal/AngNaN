2be381352e03895a21dfb136d9174d1f97917a0c
Satpal Tanan <satpal.tanan@kelltontech.com>
2018-11-29 13:01:17 +0530

    Home Slider Component Done

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlidesComponent } from './components/slides/slides.component';
 import { HomeComponent } from './home.component';
-
+import { OwlModule } from 'ngx-owl-carousel';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    OwlModule
   ],
   declarations: [SlidesComponent, HomeComponent]
 })
