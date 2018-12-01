27c09606a7136c1d2697c80f502212a560702b4c
Satpal Tanan <tanansatpal@gmail.com>
2018-12-02 00:01:30 +0530

    Header fixes

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  homeHeader: boolean = false;
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+    this.homeHeader = this.router.url === '/';
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.homeHeader = val.url === '/';
+      }
+    });
   }
 
 }
