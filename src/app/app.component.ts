49c5cf72a54c9781b3e1f68e16c77667333acff6
Satpal Tanan <tanansatpal@gmail.com>
2018-12-01 23:08:44 +0530

    Scrolltop button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngHippo';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    // ------------------------------------------------------- //
+    //   Scroll to top button
+    // ------------------------------------------------------ //
+
+   /* $(window).on('scroll', function () {
+      if ($(window).scrollTop() >= 1500) {
+        $('#scrollTop').fadeIn();
+      } else {
+        $('#scrollTop').fadeOut();
+      }
+    });
+
+
+    $('#scrollTop').on('click', function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1000);
+    });*/
+  }
 }
