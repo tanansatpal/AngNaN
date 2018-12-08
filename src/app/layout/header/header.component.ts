46ffee469e433426150a1f3756817ac6771d7d8c
Satpal Tanan <tanansatpal@gmail.com>
2018-12-09 00:35:22 +0530

    Header link change

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { getAuthStatus } from '@app/auth/reducers/selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +12,14 @@ import { Router, NavigationEnd } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   homeHeader: boolean = false;
+  isAuthenticated$: Observable<boolean>;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private store: Store<{ auth }>) {
   }
 
   ngOnInit() {
     this.homeHeader = this.router.url === '/';
+    this.isAuthenticated$ = this.store.pipe(select(getAuthStatus));
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
         this.homeHeader = val.url === '/';
