e77809b4d9181439b1b1469bb71c927dfeb2d9ea
Satpal Tanan <tanansatpal@gmail.com>
2018-12-09 19:36:24 +0530

    Header fix on scroll

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { getAuthStatus } from '@app/auth/reducers/selectors';
 import { Observable } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +15,7 @@ export class HeaderComponent implements OnInit {
   homeHeader = false;
   isAuthenticated$: Observable<boolean>;
 
-  constructor(private router: Router, private store: Store<{ auth }>) {
+  constructor(private router: Router, @Inject(DOCUMENT) document, private store: Store<{ auth }>) {
   }
 
   ngOnInit() {
@@ -25,6 +26,18 @@ export class HeaderComponent implements OnInit {
         this.homeHeader = val.url === '/';
       }
     });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+    const topbar = document.getElementById('topbar').offsetHeight;
+    if (window.pageYOffset > topbar) {
+      const element = document.getElementById('navbar');
+      element.classList.add('fixed-top');
+    } else {
+      const element = document.getElementById('navbar');
+      element.classList.remove('fixed-top');
+    }
   }
 
 }
