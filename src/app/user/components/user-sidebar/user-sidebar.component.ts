df1300f1e14cf1708e26a750df35581116459f48
Satpal Tanan <satpal.tanan@kelltontech.com>
2018-12-11 12:00:26 +0530

    Customer sidebar fix

--- src/app/user/components/user-sidebar/user-sidebar.component.ts
+++ src/app/user/components/user-sidebar/user-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-sidebar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-sidebar.component.scss']
 })
 export class UserSidebarComponent implements OnInit {
+
+  @Input() sidebarSections: any;
 
   constructor() {
   }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,11 +7,30 @@ import { UserService } from '@shared/services';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  sidebarSections: any;
 
   constructor(private userService: UserService) {
   }
 
   ngOnInit() {
+    this.sidebarSections = [
+      {
+        url: '/user/orders',
+        title: 'Orders',
+        icon: '#paper-bag-1',
+        extra: `<div class="badge badge-pill badge-dark font-weight-normal px-3">5</div>`
+      },
+      {
+        url: '/user/profile',
+        title: 'Profile',
+        icon: '#male-user-1'
+      },
+      {
+        url: '/user/address',
+        title: 'Addresses',
+        icon: '#navigation-map-1'
+      }
+    ];
   }
 
 }
