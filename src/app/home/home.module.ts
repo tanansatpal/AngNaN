9e0de001eff9a98801f15c402ae6dae274dc8fcd
Satpal Tanan <tanansatpal@gmail.com>
2018-12-16 00:57:41 +0530

    Home page section added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { BrandsComponent } from './components/brands/brands.component';
 import { SiteService } from '@shared/services/site.service';
+import { BannerComponent } from './components/banner/banner.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
   imports: [
@@ -13,7 +15,7 @@ import { SiteService } from '@shared/services/site.service';
     HomeRoutingModule,
     ComponentModule
   ],
-  declarations: [SlidesComponent, HomeComponent, BrandsComponent],
+  declarations: [SlidesComponent, HomeComponent, BrandsComponent, BannerComponent, CategoriesComponent],
   providers: [SiteService]
 })
 export class HomeModule {
