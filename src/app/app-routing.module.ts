5c0d2221d7710b52b821e64ff36acfccceba231b
Satpal Tanan <tanansatpal@gmail.com>
2019-02-20 10:07:59 +0000

    Preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,51 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadingStrategy, Route } from '@angular/router';
 import { UserGuard } from '@app/user/guards/user.guard';
+import { Observable, of } from 'rxjs';
+
+export class PreloadSelectedModulesList implements PreloadingStrategy {
+  preload(route: Route, load: Function): Observable<any> {
+    return route.data && route.data.preload ? load() : of(null);
+  }
+}
 
 const APP_ROUTES: Routes = [
   {
     path: '',
     loadChildren: './home/home.module#HomeModule',
+    data: { preload: true }
   },
   {
     path: 'auth',
     loadChildren: './auth/auth.module#AuthModule',
+    data: { preload: true }
   },
   {
     path: 'checkout',
-    loadChildren: './checkout/checkout.module#CheckoutModule',
+    loadChildren: './checkout/checkout.module#CheckoutModule'
   },
   {
     path: 'user',
     canLoad: [UserGuard],
-    loadChildren: './user/index#UserModule',
+    loadChildren: './user/index#UserModule'
   },
   {
     path: 'cart',
-    loadChildren: './cart/index#CartModule',
+    loadChildren: './cart/index#CartModule'
   },
   {
     path: 'product',
-    loadChildren: './product/index#ProductModule',
+    loadChildren: './product/index#ProductModule'
   },
   {
     path: 'category',
-    loadChildren: './category/index#CategoryModule',
+    loadChildren: './category/index#CategoryModule'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(APP_ROUTES)],
+  imports: [RouterModule.forRoot(APP_ROUTES, { preloadingStrategy: PreloadSelectedModulesList })],
+  providers: [PreloadSelectedModulesList],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
