d0e0f3282f1ccbaea47bb724467598dd5187db84
Satpal Tanan <tanansatpal@gmail.com>
2019-05-26 17:12:07 +0530

    Auth test case

--- src/app/shared/services/auth.service.spec.ts
+++ src/app/shared/services/auth.service.spec.ts
@@ -1,12 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { StoreModule } from '@ngrx/store';
+import { AuthReducer } from '@app/auth/reducers/auth.reducer';
+import { Observable, of } from 'rxjs';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AuthService;
+  const loginSpy = jasmine.createSpyObj('AuthService', ['login']);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, StoreModule.forRoot(AuthReducer)],
+      providers: [{ provide: AuthService, useValue: loginSpy }]
+    });
+    service = TestBed.get(AuthService);
+  });
 
   it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
+  });
+
+  it('should login', () => {
+    const user = { username: 'kucano@travala10.com', password: 'password' };
+    const stubValue = of({ ...user, first_name: 'Oliver', last_name: 'Queen' });
+    loginSpy.login.and.returnValue(stubValue);
+    service.login(user).subscribe(response => {
+      expect(response.first_name).toBe('Oliver');
+      expect(response.last_name).toBe('Queen');
+      expect(service.login.calls.count()).toBe(1);
+    });
+
+    expect(service);
   });
 });
